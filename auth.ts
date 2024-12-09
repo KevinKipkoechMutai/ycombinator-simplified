33262e07a08e2a9dc4fb53adf95fa0b5c852a44e
kevinmutai <mutaikevinkip.05@gmail.com>
2024-12-09 19:06:00 +0300

    update auth options

--- auth.ts
+++ auth.ts
@@ -1,6 +1,9 @@
 import NextAuth from "next-auth"
 import GitHub from "next-auth/providers/github"
 
-export const { handlers, auth, signIn, signOut } = NextAuth(config: {
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
     providers: [GitHub],
+    secret: process.env.NEXT_AUTH_SECRET,
+    trustHost: true,
 })
