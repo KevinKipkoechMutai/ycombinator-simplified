2f08b3851ad3d54d9e220183d743e0a90d84e078
kevinmutai <mutaikevinkip.05@gmail.com>
2024-12-11 07:44:00 +0300

    complete hero search

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,25 @@
-import SearchForm from "../components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+import SearchForm from "../../components/SearchForm";
 
-export default function Home() {
+export default async function Home( {searchParams}: {
+  searchParams: Promise<{ query?: string }>
+} ) {
+
+  const query = (await searchParams).query
+
+  const posts = [
+    {
+      _createdAt: new Date(),
+      views: 55,
+      author: { _id: 1, name: 'Kevin K.' },
+      _id: 1,
+      description: 'This is a description',
+      image: 'https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg',
+      category: 'Lizards',
+      title: 'Cool Lizard'
+    }
+  ]
+
   return (
     <>
       <section className="pink_container">
@@ -13,7 +32,27 @@ export default function Home() {
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions
         </p>
 
-        <SearchForm/>
+        <SearchForm query={query}/>
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {
+            query ? `Search results for "${query}"` : "All Startups"
+          }
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {
+            posts?.length > 0 ? (
+              posts.map((post: StartupCardType, index: number) => (
+                <StartupCard key={post._id} post={post}/>
+              ))
+              ) : (
+              <p className="no-results">No Startups Found</p>
+            )
+          }
+        </ul>
       </section>
       
     </>
