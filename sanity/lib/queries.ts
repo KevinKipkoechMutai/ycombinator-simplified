bb1da58997a158478511e9fa42a78aefee0a4c43
kevinmutai <mutaikevinkip.05@gmail.com>
2024-12-16 23:03:57 +0300

    update queries

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,14 +1,18 @@
 import { defineQuery } from "next-sanity";
 
-export const STARTUPS_QUERY = defineQuery(`
-    *[_type == "startup" && defined(slug.current)] | order(_createdAt desc) {_id,
-        title,
-        slug,
-        _createdAt,
-        author -> {
-          _id, name, image, bio
-        },
-        views,
-        description,
-        category,
-        image}`)
+export const STARTUPS_QUERY = defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || title match $search || category match $search || author->name match $search] | order(_createdAt desc) {
+  _id, 
+  title, 
+  slug,
+  _createdAt,
+  author -> {
+    _id, name, image, bio
+  }, 
+  views,
+  description,
+  category,
+  image,
+}`);
+
+// Ensure to provide the $search parameter when executing the query
+// Example: const results = await client.fetch(STARTUPS_QUERY, { search: yourSearchValue });
